39ff97149fd98199ce631238c49b3788a5e856b1
vikram kumar <vikramkumar0120arav@gmail.com>
2025-01-19 10:44:10 +0530

    initial phase

---
+++ Portfolio/src/Components/Cv.jsx
@@ -0,0 +1,8 @@
+import { div } from "framer-motion/client";
+import React from "react";
+function Cv(){
+    return (
+        <div className="bg-red-900 w-full h-full">Dowloand Cv</div>
+    )
+}
+export default Cv;
